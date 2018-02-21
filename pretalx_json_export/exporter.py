4273d7b9080fc015ea22cd70fa30f3af3495e03a
Tobias Kunze <rixx@cutebit.de>
2018-02-22 00:26:28 +0100

    Add review JSON exporter

---
+++ pretalx_json_export/exporter.py
@@ -0,0 +1,36 @@
+import json
+
+from i18nfield.rest_framework import I18nAwareModelSerializer
+from i18nfield.utils import I18nJSONEncoder
+from rest_framework import serializers
+
+from pretalx.api.serializers.question import AnswerSerializer
+from pretalx.api.serializers.submission import SubmissionSerializer
+from pretalx.common.exporter import BaseExporter
+from pretalx.submission.models import Review
+
+
+class ReviewSerializer(I18nAwareModelSerializer):
+    submission = SubmissionSerializer()
+    answers = AnswerSerializer(many=True)
+    user = serializers.SlugRelatedField(slug_field='nick', read_only=True)
+
+    class Meta:
+        model = Review
+        fields = (
+            'id', 'submission', 'user', 'text', 'score', 'override_vote',
+            'created', 'updated', 'answers',
+        )
+
+
+class ReviewExporter(BaseExporter):
+    identifier = 'pretalx-json-exporter-reviews'
+    verbose_name = 'Reviews (JSON)'
+    icon = '{ }'
+    public = False
+
+    def render(self, **kwargs):
+        reviews = Review.objects.filter(submission__event=self.event)
+        data = ReviewSerializer(reviews, many=True).data
+        data = json.dumps(data, cls=I18nJSONEncoder)
+        return f'{self.event.slug}-reviews.json', 'application/json', data
--- pretalx_json_export/signals.py
+++ pretalx_json_export/signals.py
@@ -1,1 +1,9 @@
-# Register your receivers here
+from django.dispatch import receiver
+
+from pretalx.common.signals import register_data_exporters
+
+
+@receiver(register_data_exporters, dispatch_uid="exporter_json_reviews")
+def register_data_exporter(sender, **kwargs):
+    from .exporter import ReviewExporter
+    return ReviewExporter
