c0922c677f147f9deb61a4b9019452a98d451013
Tobias Kunze <rixx@cutebit.de>
2018-02-21 23:34:58 +0100

    Initial commit

---
+++ pretalx_json_export/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_json_export'
+    verbose_name = 'More JSON exports for pretalx'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('More JSON exports for pretalx')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy('A range of additional JSON exports (questions, reviews, …)')
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretalx_json_export.PluginApp'
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1, interactive=False)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='pretalx-json-export',
+    version='0.0.0',
+    description='A range of additional JSON exports (questions, reviews, …)',
+    long_description=long_description,
+    url='https://github.com/rixx/pretalx-json-export',
+    author='Tobias Kunze',
+    author_email='rixx@cutebit.de',
+    license='Apache Software License',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretalx.plugin]
+pretalx_json_export=pretalx_json_export:PretalxPluginMeta
+""",
+)
